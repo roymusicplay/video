408b942504fbb2b7fcc946c7eb16182304da002c
levina <82658782+levina-lab@users.noreply.github.com>
2021-09-07 16:38:42 +0700

    seed

--- vcbot/player.py
+++ vcbot/player.py
@@ -80,7 +80,7 @@ class Player:
                     frame_rate=fps,
                 ),
             ),
-            stream_type=StreamType().pulse_stream,
+            stream_type=StreamType().local_stream,
         )
         now_playing.append(self._current_chat)
 
@@ -96,9 +96,9 @@ class Player:
             InputVideoStream(
                 video,
                 VideoParameters(
-                    width=height,
-                    height=width,
-                    frame_rate=fps,
+                    width=1280,
+                    height=720,
+                    frame_rate=25,
                 )
             )
         )
